00b2d3589137db074aa8279d12aa2b8975293693
hengyuan-hu <kevin.huhy@gmail.com>
2017-03-14 19:22:21 -0400

    add hmc.

---
+++ hmc.py
@@ -0,0 +1,144 @@
+from __future__ import print_function
+
+import os
+import time
+import numpy as np
+import torch
+from torch.autograd import Variable
+
+import utils
+import torch_utils
+
+
+def kinetic(vel):
+    batch_size = vel.size()[0]
+    vel_sq = torch.mul(vel, vel)
+    return 0.5 * vel_sq.view(batch_size, -1).sum(1)
+
+
+def hamil(pos, vel, potential_fn):
+    # utils.assert_eq(type(pos), torch.cuda.FloatTensor)
+    # utils.assert_eq(type(vel), torch.cuda.FloatTensor)
+    potential = potential_fn(Variable(pos)).data
+    return potential + kinetic(vel)
+
+
+def mh_accept(prev_energy, next_energy):
+    """should take tensor as input."""
+    uniform = torch.cuda.FloatTensor(*prev_energy.size()).uniform_()
+    energy_diff = prev_energy - next_energy
+    accept = energy_diff.exp_() >= uniform
+    return accept
+
+
+def simulate_dynamics(init_pos, init_vel, step_size, num_steps, potential_fn):
+    """potential fn takes a Variable and returns a Variable."""
+    var_pos = Variable(init_pos, requires_grad=True)
+    potential = potential_fn(var_pos).sum(0)
+    potential.backward()
+    new_vel = init_vel - 0.5 * step_size * var_pos.grad.data
+    new_pos = init_pos + step_size * new_vel
+
+    for _ in xrange(num_steps - 1):
+        var_pos = Variable(new_pos, requires_grad=True)
+        # var_pos.data.copy_(new_pos)
+        potential = potential_fn(var_pos).sum(0)
+        potential.backward()
+        # print(var_pos.grad.data)
+        new_vel.add_(-step_size * var_pos.grad.data)
+        new_pos.add_(step_size * new_vel)
+
+    var_pos = Variable(new_pos, requires_grad=True)
+    potential = potential_fn(var_pos).sum(0)
+    new_vel = new_vel - 0.5 * step_size * var_pos.grad.data
+    return new_pos, new_vel
+
+
+def hmc_sample(pos, step_size, num_steps, potential_fn):
+    vel = torch.cuda.FloatTensor(*pos.size()).normal_()
+    final_pos, final_vel = simulate_dynamics(
+        pos, vel, step_size, num_steps, potential_fn)
+    accept = mh_accept(
+        hamil(pos, vel, potential_fn),
+        hamil(final_pos, final_vel, potential_fn))
+    # print(accept)
+    accept = accept.float()
+    accept_rate = accept.mean()
+
+    # print(type(final_pos))
+    accept = accept.view(-1, 1).expand_as(final_pos)
+    # print(accept)
+    new_pos = final_pos * accept + pos * (1 - accept)
+    return new_pos, accept_rate
+
+
+# class HMCSampler(object):
+#     def __init__(self, potential_fn, step_size, target_accept_rate, num_steps)
+
+
+def sampler_on_nd_gaussian(burnin, num_chains, num_samples, dim):
+    np.random.seed(666)
+    mu = np.random.uniform(0, 20, dim).astype(np.float32)
+    cov = np.random.uniform(0, 10, (dim, dim)).astype(np.float32)
+    cov = np.dot(cov, cov.T)
+    cov = cov / cov.max()
+    # cov = np.identity(dim).astype(np.float32)
+    cov = (cov + cov.T) / 2.
+    cov[np.arange(dim), np.arange(dim)] = 1.0
+    cov_inv = np.linalg.inv(cov)
+
+    def gaussian_energy(x):
+        cuda_mu = torch.from_numpy(mu).cuda()
+        cuda_mu = Variable(cuda_mu.view(1, -1).expand(x.size()[0], cuda_mu.size()[-1]),
+                           requires_grad=False)
+        cuda_cov_inv = Variable(torch.from_numpy(cov_inv).cuda(),
+                                requires_grad=False)
+
+        e = torch.sum(
+            torch.mul(
+                torch.mm(x-cuda_mu, cuda_cov_inv),
+                x-cuda_mu),
+            1)
+        # print(e.mean(0).size())
+        return 0.5 * e # .mean(0)
+
+    new_pos = torch.cuda.FloatTensor(num_chains, dim).normal_()
+    step_size = 0.01
+    num_steps = 20
+
+    for _ in xrange(burnin):
+        new_pos, _, = hmc_sample(new_pos, step_size, num_steps, gaussian_energy)
+
+    samples = []
+    accept_rate = []
+    for _ in xrange(num_samples):
+        new_pos, rate = hmc_sample(new_pos, step_size, num_steps, gaussian_energy)
+        # print(type(new_pos))
+        samples.append(new_pos.cpu().numpy())
+        accept_rate.append(rate)
+
+    samples = np.array(samples)
+    print(samples.shape)
+    samples = samples.T.reshape(dim, -1).T
+    print(samples.shape)
+
+    print('****** TARGET VALUES ******')
+    print('target mean:', mu)
+    print('target cov:\n', cov)
+
+    print('****** EMPIRICAL MEAN/COV USING HMC ******')
+    print('empirical mean: ', samples.mean(0))
+    print('empirical_cov:\n', np.cov(samples.T))
+
+    print('****** HMC INTERNALS ******')
+    # print('final stepsize:', final_stepsize
+    print('avg acceptance_rate', np.array(accept_rate).mean())
+
+    print('DIFF')
+    print(np.abs(cov - np.cov(samples.T)).sum())
+    # print(cov.sum() - np.cov(samples.T).sum())
+
+
+if __name__ == '__main__':
+    torch.backends.cudnn.benckmark = True
+    sampler_on_nd_gaussian(1000, 10, 1000, 5)
